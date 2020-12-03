74897af4124aa3bd27889e2f707ef9fe082c46f2
Sven Thiel <sven.thiel@uni-jena.de>
2020-12-03 21:55:55 +0100

    add files and libs

---
+++ Entities/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manchu.Entities
+{
+    public class Link
+    {
+        public Guid Id { get; set; }
+        public string PatientId { get; set; }
+        public List<DateTimeOffset> Visits { get; set; }
+
+        public Link()
+        {
+            Visits = new List<DateTimeOffset>();
+        }
+    }
+}
---
+++ Services/LinkService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Manchu.Entities;
+
+namespace Manchu.Services
+{
+    public interface ILinkService
+    {
+        Guid Create(string patientId);
+        bool Delete(Guid id);
+        bool Delete(string patientId);
+        Link FindById(Guid id);
+        Link FindByPatientId(string patientId);
+        IQueryable<Link> Query();
+    }
+
+    public class LinkService : ILinkService
+    {
+
+    }
+}
