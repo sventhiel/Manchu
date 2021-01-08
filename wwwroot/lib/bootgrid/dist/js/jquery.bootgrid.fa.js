4ee5c3c5d2f93cfeb86491eeac0d9ad4e3ac059b
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-08 22:15:37 +0100

    clean-up assets

--- wwwroot/lib/bootgrid/dist/js/jquery.bootgrid.fa.js
+++ wwwroot/lib/bootgrid/dist/js/jquery.bootgrid.fa.js
@@ -1,10 +1,9 @@
-/*! 
+/*!
  * jQuery Bootgrid v1.3.1 - 09/11/2015
  * Copyright (c) 2014-2015 Rafael Staib (http://www.jquery-bootgrid.com)
  * Licensed under MIT http://www.opensource.org/licenses/MIT
  */
-;(function ($, window, undefined)
-{
+; (function ($, window, undefined) {
     /*jshint validthis: true */
     "use strict";
 
@@ -15,5 +14,5 @@
         iconRefresh: "fa-refresh",
         iconSearch: "fa-search",
         iconUp: "fa-sort-asc"
-});
+    });
 })(jQuery, window);
