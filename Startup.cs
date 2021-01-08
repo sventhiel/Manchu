7ce68b2df6c342a1009c59a162a6937581ea038d
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-08 22:16:46 +0100

    add singleton for LiteDB connection string, clean-up

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using LiteDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +19,8 @@ namespace Manchu
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ConnectionString>(new ConnectionString(Configuration.GetConnectionString("Manchu")));
             services.AddControllersWithViews();
         }
 
