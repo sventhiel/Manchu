d795ac5a7fa63fc5ec941e1017cff0602cfbd896
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-08 22:12:28 +0100

    add new entities for patients and visits (views of the audio file)

---
+++ Entities/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manchu.Entities
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Reference { get; set; }
+        public bool IsActive { get; set; }
+        public List<Visit> Visits { get; set; }
+
+        public Patient()
+        {
+            IsActive = true;
+            Visits = new List<Visit>();
+        }
+    }
+}
---
+++ Entities/Visit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Manchu.Entities
+{
+    public class Visit
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset Stop { get; set; }
+        public bool WasCompleted { get; set; }
+
+        public Visit()
+        {
+            Start = DateTimeOffset.UtcNow;
+            Stop = DateTimeOffset.MinValue;
+            WasCompleted = false;
+        }
+    }
+}
