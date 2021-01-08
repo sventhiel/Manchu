cca499a104832f68c484acb87d84bbf52dbc45c9
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-08 22:13:15 +0100

    add first model for patients inside a grid

---
+++ Models/PatientModels.cs
@@ -0,0 +1,40 @@
+﻿using Manchu.Entities;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Manchu.Models
+{
+    public class PatientGridItemModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string QRCode { get; set; }
+
+        public static PatientGridItemModel Convert(Patient patient)
+        {
+            var base64 = "";
+
+            using (var ms = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{HttpContext.Request.PathBase}/{patient.Id}", QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                using (var bitmap = new Bitmap(qrCode.GetGraphic(20)))
+                {
+                    bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    base64 = System.Convert.ToBase64String(ms.GetBuffer()); //Get Base64
+                }
+            }
+
+            return new PatientGridItemModel()
+            {
+                Id = patient.Id,
+                Name = patient.Name,
+                QRCode = base64
+            };
+        }
+    }
+}
