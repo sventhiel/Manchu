2454901654c14e985f77d97556e77978a4d0eae5
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-14 09:53:18 +0100

    add model for grid item

--- Models/VisitModels.cs
+++ Models/VisitModels.cs
@@ -1,6 +1,26 @@
-﻿namespace Manchu.Models
+﻿using Manchu.Entities;
+using System;
+
+namespace Manchu.Models
 {
-    public class VisitModels
+    public class VisitGridItemModel
     {
+        public Guid Id { get; set; }
+        public Guid PatientId { get; set; }
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset Stop { get; set; }
+        public bool Completed { get; set; }
+
+        public static VisitGridItemModel Convert(Visit visit)
+        {
+            return new VisitGridItemModel()
+            {
+                Id = visit.Id,
+                PatientId = visit.PatientId,
+                Start = visit.Start,
+                Stop = visit.Stop,
+                Completed = visit.Completed
+            };
+        }
     }
 }
