f672b463e264c8fbcbe50ea9bc48c12ead99635b
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-01-29 10:21:59 +0100

    update of property names, removal & creation of further ones

--- Entities/Visit.cs
+++ Entities/Visit.cs
@@ -7,14 +7,18 @@ namespace Manchu.Entities
         public Guid Id { get; set; }
         public Guid PatientId { get; set; }
         public DateTimeOffset Start { get; set; }
-        public DateTimeOffset Stop { get; set; }
+        public int Breaks { get; set; }
+        public TimeSpan Position { get; set; }
+        public DateTimeOffset End { get; set; }
         public bool Completed { get; set; }
 
         public Visit()
         {
             Start = DateTimeOffset.UtcNow;
-            Stop = DateTimeOffset.MinValue;
+            Position = TimeSpan.MinValue;
+            End = DateTimeOffset.MinValue;
             Completed = false;
+            Breaks = 0;
         }
     }
 }
