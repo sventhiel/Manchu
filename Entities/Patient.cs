7ec5d284acb0104d2c61471eda329f47618f8ca0
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-02-18 19:41:00 +0100

    change id property

--- Entities/Patient.cs
+++ Entities/Patient.cs
@@ -4,16 +4,15 @@ namespace Manchu.Entities
 {
     public class Patient
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
+        public Guid Code { get; set; }
         public int Number { get; set; }
         public string Name { get; set; }
         public string Reference { get; set; }
-        public bool IsActive { get; set; }
 
         public Patient()
         {
-            Number = 0;
-            IsActive = true;
+            Code = Guid.NewGuid();
         }
     }
 }
