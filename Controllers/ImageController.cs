3ef0317e2696d932318589b2917fe2119b54a949
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-02-20 21:06:06 +0100

    functions to create qr code image

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,67 @@
+﻿using LazZiya.ImageResize;
+using LiteDB;
+using Manchu.Services;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manchu.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ConnectionString _connectionString;
+
+        public ImageController(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void CreateQRCode(int id)
+        {
+            var patientService = new PatientService(_connectionString);
+
+            var patient = patientService.FindById(id);
+
+            var iwmOps = new ImageWatermarkOptions
+            {
+                Location = TargetSpot.BottomMiddle,
+                Margin = 550
+            };
+
+            var twmOps = new TextWatermarkOptions
+            {
+                Location = TargetSpot.BottomRight,
+                FontSize = 32,
+                FontName = "Lato",
+                TextColor = Color.Black
+                
+            };
+
+            using (var img = Image.FromFile($"./wwwroot/media/images/manchu.png"))
+            {
+                var url = $"{Request.Scheme}://{Request.Host.Value}/Home/Index";
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{url}?code={patient.Code}", QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                img.AddImageWatermark(qrCode.GetGraphic(25, Color.Black, Color.Transparent, true), iwmOps)
+                    .ScaleByWidth(500)
+                    .AddTextWatermark($"{patient.Id}", twmOps)
+                    .SaveAs($"./wwwroot/media/codes/{patient.Code}.png");
+            }
+
+            
+        }
+    }
+}
