ebd19b829bee299522526a70403c495b58a03cff
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-02-26 10:29:19 +0100

    remove qr code properties

--- Models/PatientModels.cs
+++ Models/PatientModels.cs
@@ -18,33 +18,16 @@ namespace Manchu.Models
         public Guid Code { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string QRCode { get; set; }
+        public string Reference { get; set; }
 
         public static PatientGridItemModel Convert(Patient patient, string url)
         {
-            var base64 = "";
-
-            //using (var ms = new MemoryStream())
-            //{
-            //    Bitmap baseImage = (Bitmap)Image.FromFile(@"~/media/images/manchu.png");
-
-            //    QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            //    QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{url}?id={patient.Id}", QRCodeGenerator.ECCLevel.Q);
-            //    QRCode qrCode = new QRCode(qrCodeData);
-
-            //    using (var bitmap = new Bitmap(qrCode.GetGraphic(20)))
-            //    {
-            //        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            //        base64 = System.Convert.ToBase64String(ms.GetBuffer()); //Get Base64
-            //    }
-            //}
-
             return new PatientGridItemModel()
             {
                 Id = patient.Id,
                 Code = patient.Code,
                 Name = patient.Name,
-                QRCode = base64
+                Reference = patient.Reference
             };
         }
     }
