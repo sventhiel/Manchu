e131a547ccebb8eda03ab21c65180066696a7b3e
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-03-30 23:29:56 +0200

    adapt model related to the entity

--- Models/PatientModels.cs
+++ Models/PatientModels.cs
@@ -1,33 +1,19 @@
 ﻿using Manchu.Entities;
-using QRCoder;
 using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
 
 namespace Manchu.Models
 {
-    public class CreatePatientModel
-    {
-        public string Name { get; set; }
-        public string Reference { get; set; }
-    }
     public class PatientGridItemModel
     {
         public Guid Code { get; set; }
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Reference { get; set; }
 
         public static PatientGridItemModel Convert(Patient patient)
         {
             return new PatientGridItemModel()
             {
                 Id = patient.Id,
-                Code = patient.Code,
-                Name = patient.Name,
-                Reference = patient.Reference
+                Code = patient.Code
             };
         }
     }
