168f1d8eb2028294522162851b31d6ce0a5d7a49
Sven Thiel <m6thsv2@googlemail.com>
2023-06-08 10:34:30 +0200

    add controller with login/logout functionalities

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Manchu.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Manchu.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,8 +15,53 @@ namespace Manchu.Controllers
 {
     public class AccountController : Controller
     {
+        private List<Admin> _admins;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _admins = configuration.GetSection("Admins").Get<List<Admin>>();
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var admin = _admins.Find(a => a.Name.Equals(model.Username));
+
+            if (admin != null && admin.Password.Equals(model.Password))
+            {
+                var claims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.Name, model.Username),
+                    };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult AccessDenied()
         {
             return View();
         }
