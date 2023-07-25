500b5169963107210e9c4364a0ac0a84c0a90679
Sven Thiel <m6thsv2@googlemail.com>
2023-07-25 08:46:55 +0200

    modify patient service to support free choice of number

--- Services/PatientService.cs
+++ Services/PatientService.cs
@@ -1,12 +1,15 @@
 ﻿using LiteDB;
 using Manchu.Entities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Manchu.Services
 {
     public interface IPatientService
     {
-        int Create();
+        int? Create(Guid? code, int? number);
+
         bool Delete(int id);
 
         Patient FindById(int id);
@@ -25,31 +28,23 @@ namespace Manchu.Services
             _connectionString = connectionString;
         }
 
-        public int Create()
+        public int? Create(Guid? code, int? number)
         {
             using (var db = new LiteDatabase(_connectionString))
             {
-                Patient patient = new Patient
-                {
-                    Code = Guid.NewGuid()
-                };
-
                 var col = db.GetCollection<Patient>("patients");
 
-                return col.Insert(patient);
-            }
-        }
-
-        public int Create(Guid code)
-        {
-            using (var db = new LiteDatabase(_connectionString))
-            {
                 Patient patient = new Patient
                 {
-                    Code = code
+                    Code = code ?? Guid.NewGuid(),
+                    Number = number ?? col.Max(x => x.Number) + 1
                 };
 
-                var col = db.GetCollection<Patient>("patients");
+                if (col.Exists(p => p.Code == code))
+                    return null;
+
+                if (col.Exists(p => p.Number == patient.Number))
+                    return null;
 
                 return col.Insert(patient);
             }
@@ -62,6 +57,22 @@ namespace Manchu.Services
                 var col = db.GetCollection<Patient>("patients");
 
                 return col.Delete(id);
+            }
+        }
+
+        public List<Patient> FindAll()
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var patients = new List<Patient>();
+                var col = db.GetCollection<Patient>("patients");
+
+                foreach (var pat in col.FindAll())
+                {
+                    patients.Add(pat);
+                }
+
+                return patients;
             }
         }
 
@@ -81,7 +92,27 @@ namespace Manchu.Services
             {
                 var col = db.GetCollection<Patient>("patients");
 
-                return col.FindOne(p => p.Code == code);
+                var patients = col.Find(p => p.Code == code);
+
+                if (patients.Count() != 1)
+                    return null;
+
+                return patients.First();
+            }
+        }
+
+        public Patient FindByNumber(int number)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Patient>("patients");
+
+                var patients = col.Find(p => p.Number == number);
+
+                if (patients.Count() != 1)
+                    return null;
+
+                return patients.First();
             }
         }
 
@@ -92,6 +123,16 @@ namespace Manchu.Services
                 var col = db.GetCollection<Patient>("patients");
 
                 return col.Query();
+            }
+        }
+
+        public bool Update(Patient patient)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Patient>("patients");
+
+                return col.Update(patient);
             }
         }
     }
