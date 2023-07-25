dd56cf107dae9cd9057dea44d435aed5650581cc
Sven Thiel <m6thsv2@googlemail.com>
2023-07-25 08:47:52 +0200

    minor changes in models

--- Models/PatientModels.cs
+++ Models/PatientModels.cs
@@ -17,4 +17,36 @@ namespace Manchu.Models
             };
         }
     }
+
+    public class CreatePatientModel
+    {
+        public Guid? Code { get; set; }
+        public int? Number { get; set; }
+    }
+
+    public class UpdatePatientModel
+    {
+        public int Id { get; set; }
+        public Guid Code { get; set; }
+
+        public int Number { get; set; }
+    }
+
+    public class ReadPatientModel
+    {
+        public int Id { get; set; }
+        public Guid Code { get; set; }
+
+        public int Number { get; set; }
+
+        public static ReadPatientModel Convert(Patient patient)
+        {
+            return new ReadPatientModel()
+            {
+                Id = patient.Id,
+                Code = patient.Code,
+                Number = patient.Number
+            };
+        }
+    }
 }
