3229243eba913e57d26e1a5350bdd5ea6e46ffd7
Sven Thiel <m6thsv2@googlemail.com>
2023-07-25 08:48:05 +0200

    add jwt

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,20 @@
 using LiteDB;
+using Manchu.Authentication;
+using Manchu.Configurations;
+using Manchu.Filters;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 
 namespace Manchu
 {
@@ -15,35 +23,98 @@ namespace Manchu
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            JwtConfiguration = configuration.GetSection("JWT").Get<JwtConfiguration>();
         }
 
         public IConfiguration Configuration { get; }
+        public JwtConfiguration JwtConfiguration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(new ConnectionString(Configuration.GetConnectionString("Manchu")));
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo
+                var basicSecurityScheme = new OpenApiSecurityScheme
                 {
-                    Title = "Zomato API",
+                    Name = "Basic",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Basic",
+                    Description = "Provide your username and password.",
+
+                    Reference = new OpenApiReference
+                    {
+                        Id = "Basic",
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    BearerFormat = "JWT",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                options.AddSecurityDefinition(basicSecurityScheme.Reference.Id, basicSecurityScheme);
+                options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+                options.SchemaFilter<EnumSchemaFilter>();
+                options.OperationFilter<AuthorizeHeaderOperationFilter>();
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Manchu API",
                     Version = "v1",
                     Description = "Description for the API goes here.",
                     Contact = new OpenApiContact
                     {
-                        Name = "Ankush Jain",
+                        Name = "Sven Thiel",
                         Email = string.Empty,
                         Url = new Uri("https://coderjony.com/"),
                     },
                 });
             });
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            services.AddAuthentication(options =>
             {
-                options.LoginPath = "/Account/Login";
-                options.LogoutPath = "/Account/Logout";
-                options.AccessDeniedPath = "/Account/AccessDenied";
-            });
+                options.DefaultScheme = "BASIC_OR_JWT";
+                options.DefaultChallengeScheme = "BASIC_OR_JWT";
+            })
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>
+                ("Basic", null)
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = JwtConfiguration.ValidateIssuer,
+            ValidateAudience = JwtConfiguration.ValidateAudience,
+            ValidAudience = JwtConfiguration.ValidAudience,
+            ValidIssuer = JwtConfiguration.ValidIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtConfiguration.IssuerSigningKey ?? ""))
+        };
+    })
+    .AddPolicyScheme("BASIC_OR_JWT", "BASIC_OR_JWT", options =>
+    {
+        options.ForwardDefaultSelector = context =>
+        {
+            string authorization = context.Request.Headers[HeaderNames.Authorization];
+
+            if (!string.IsNullOrEmpty(authorization) && authorization.StartsWith("Basic ", StringComparison.InvariantCultureIgnoreCase))
+                return "Basic";
+
+            return JwtBearerDefaults.AuthenticationScheme;
+        };
+    });
             services.AddControllersWithViews();
         }
 
@@ -74,7 +145,7 @@ namespace Manchu
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zomato API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Manchu API");
 
                 // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
                 c.RoutePrefix = string.Empty;
