f520c09e938444b9884311afd3cb3720e9daccfe
Sven Thiel <m6thsv2@googlemail.com>
2023-07-25 08:47:35 +0200

    support user login/logout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,12 @@
-﻿using Manchu.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Manchu.Entities;
+using Manchu.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Manchu.Entities;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Authorization;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Manchu.Controllers
 {
