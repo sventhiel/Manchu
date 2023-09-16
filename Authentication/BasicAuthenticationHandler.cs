12306265f8f4801a0a6baa0457fe87535479f7b3
Sven Thiel <m6thsv2@googlemail.com>
2023-09-16 16:02:22 +0200

    update project

--- Authentication/BasicAuthenticationHandler.cs
+++ Authentication/BasicAuthenticationHandler.cs
@@ -31,7 +31,7 @@ namespace Manchu.Authentication
             _admins = configuration.GetSection("Admins").Get<List<Admin>>();
         }
 
-        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
             var authHeader = Request.Headers["Authorization"].ToString();
             if (authHeader != null && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
@@ -54,18 +54,18 @@ namespace Manchu.Authentication
 
                     var identity = new ClaimsIdentity(claims, "Basic");
                     var claimsPrincipal = new ClaimsPrincipal(identity);
-                    return AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name));
+                    return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
                 }
 
                 Response.StatusCode = 401;
                 Response.Headers.Add("www-authenticate", "Basic Authorization");
-                return AuthenticateResult.Fail(new UnauthorizedAccessException());
+                return Task.FromResult(AuthenticateResult.Fail(new UnauthorizedAccessException()));
             }
             else
             {
                 Response.StatusCode = 401;
                 Response.Headers.Add("www-authenticate", "Basic Authorization");
-                return AuthenticateResult.Fail(new UnauthorizedAccessException());
+                return Task.FromResult(AuthenticateResult.Fail(new UnauthorizedAccessException()));
             }
         }
     }
--- Filters/EnumSchemaFilter.cs
+++ Filters/EnumSchemaFilter.cs
@@ -16,8 +16,8 @@ namespace Manchu.Filters
                 model.Enum.Clear();
                 foreach (string enumName in Enum.GetNames(context.Type))
                 {
-                    System.Reflection.MemberInfo? memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
-                    EnumMemberAttribute? enumMemberAttribute = memberInfo?.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>().FirstOrDefault();
+                    System.Reflection.MemberInfo memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
+                    EnumMemberAttribute enumMemberAttribute = memberInfo?.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>().FirstOrDefault();
                     string label = enumMemberAttribute == null || string.IsNullOrWhiteSpace(enumMemberAttribute.Value)
                      ? enumName
                      : enumMemberAttribute.Value;
