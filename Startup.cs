5e8618b54da5a0df942b4ba62f5acc861fce09d3
Sven Thiel <m6thsv2@googlemail.com>
2023-09-20 20:57:33 +0200

    code-maid clean up

--- Startup.cs
+++ Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace Manchu
 {
